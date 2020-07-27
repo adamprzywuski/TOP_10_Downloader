f39ab77ea5169e84442a7c1ebf7a9809a9183773
Adam Przywuski <adamprzywuski98@gmail.com>
2020-07-27 21:28:26 +0200

    Lesson 95

--- app/src/main/java/com/example/top10downloader/MainActivity.kt
+++ app/src/main/java/com/example/top10downloader/MainActivity.kt
@@ -6,22 +6,32 @@ import android.os.Bundle
 import android.util.Log
 
 class MainActivity : AppCompatActivity() {
-    private val TAG="MainActivity"
+    private val TAG="Mainactivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         Log.d(TAG,"OnCreate called")
+
+        val DownloadData=DownloadData()
+        DownloadData.execute("URL goes here")
+        Log.d(TAG,"onCreate:Done")
     }
-    private inner class DownloadData: AsyncTask<String, Void, String>()
-    {
-        private val TAG="DownloadData"
-        override fun onPostExecute(result: String?) {
-            super.onPostExecute(result)
-        }
 
-        override fun doInBackground(vararg params: String?): String {
-            TODO("Not yet implemented")
-        }
+    companion object {
+        private class DownloadData: AsyncTask<String, Void, String>()
+        {
+            private val TAG="DownloadData"
+            override fun onPostExecute(result: String?) {
+                super.onPostExecute(result)
+                Log.d(TAG,"OnPostExcectuive parameter is $result")
+            }
 
+            override fun doInBackground(vararg params: String?): String {
+                Log.d(TAG,"doInBackground: starts with ${params[0]}")
+                return "DoInBackground completed"
+            }
+
+        }
     }
 }
